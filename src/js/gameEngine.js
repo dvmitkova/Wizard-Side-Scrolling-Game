da992cc258f7c3e8042a9f7c60c86c111185b252
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-01-08 16:38:21 +0200

    Add Delay to Bug Spawn

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -2,15 +2,33 @@
 
 function start(state, game) {
     game.createWizard(state.wizard);//създаваме динамично wizard-a;
-    
-    window.requestAnimationFrame(gameLoop.bind(null, state, game))//bind свързва ф-ята
-    //с подадените параметри state и game и винаги, когато я извикаме, се подават по референция.
+
+    window.requestAnimationFrame((timestamp) => gameLoop(state, game, timestamp))//timestamp - времеви
+    //отпечатък от стартирането на animation frame-a;
 }
 
-function gameLoop(state, game) {
+function gameLoop(state, game, timestamp) {
     const { wizard } = state;//състояние;
     const { wizardElement } = game;//ДОМ изглед;
 
+    modifyWizardPosition(state, game);
+
+    //Spawn bugs
+    if (timestamp > state.bugStats.nextSpawnTimestamp) {//ако timestamp е по-голям
+        //значи е дошло време да пратим нов бъг;
+        game.createBug(state.bugStats);
+        state.bugStats.nextSpawnTimestamp = timestamp + Math.random() * state.bugStats.maxSpawnInterval;
+    }
+    
+    //RenderMove wizard - render DOM - местене на човечето по екрана;
+    wizardElement.style.left = wizard.posX + 'px';
+    wizardElement.style.top = wizard.posY + 'px';
+
+    window.requestAnimationFrame(gameLoop.bind(null, state, game));//правим безкраен цикъл;
+}
+
+function modifyWizardPosition(state, game) {
+    const { wizard } = state;
 
     //Move wizard - state
     if (state.keys.KeyD) {//Ако е натиснат бутон D:
@@ -30,11 +48,4 @@ function gameLoop(state, game) {
         wizard.posY = Math.min(wizard.posY + wizard.speed, game.gameScreen.offsetHeight - wizard.height);
         //по-малката от двете ст-сти - втората е крайната точка на екрана;
     }
-
-
-    //RenderMove wizard - render DOM - местене на човечето по екрана;
-    wizardElement.style.left = wizard.posX + 'px';
-    wizardElement.style.top = wizard.posY + 'px';
-
-    window.requestAnimationFrame(gameLoop.bind(null, state, game));//правим безкраен цикъл;
 }
--- src/js/gameState.js
+++ src/js/gameState.js
@@ -13,8 +13,17 @@ function initState() {
             posY: startY,
             speed: 10,
         },
+        bugStats: {
+            width: 50,
+            height: 50,
+            nextSpawnTimestamp: 0,
+            maxSpawnInterval: 1500,
+        },
         keys: {
-
+            KeyA: false,
+            KeyS: false,
+            KeyD: false,
+            KeyW: false,
         }
     }
 

53ffc1aeb1ba4238284b5009e1bd908fada4b2a2
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-01-08 15:32:12 +0200

    Add movement boundaries

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -14,19 +14,21 @@ function gameLoop(state, game) {
 
     //Move wizard - state
     if (state.keys.KeyD) {//Ако е натиснат бутон D:
-        state.wizard.posX += wizard.speed;
+        wizard.posX = Math.min(wizard.posX + wizard.speed, game.gameScreen.offsetWidth - wizard.width);
     }
 
     if (state.keys.KeyW) {//Ако е натиснат бутон W:
-        state.wizard.posY -= wizard.speed;
+        wizard.posY = Math.max(wizard.posY - wizard.speed, 0);
+        //По-голямата ст-ст от двете - wizard.posY - wizard.speed или 0;
     }
 
     if (state.keys.KeyA) {//Ако е натиснат бутон D:
-        state.wizard.posX -= wizard.speed;
+        wizard.posX = Math.max(wizard.posX - wizard.speed, 0);
     }
 
     if (state.keys.KeyS) {//Ако е натиснат бутон W:
-        state.wizard.posY += wizard.speed;
+        wizard.posY = Math.min(wizard.posY + wizard.speed, game.gameScreen.offsetHeight - wizard.height);
+        //по-малката от двете ст-сти - втората е крайната точка на екрана;
     }
 
 

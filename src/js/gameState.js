68d332cef4ccc8b5fbec5ccc61d5ce319cb29b3f
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-01-09 09:41:10 +0200

    Move fireballs

--- src/js/gameState.js
+++ src/js/gameState.js
@@ -23,6 +23,7 @@ function initState() {
         fireball: {
             width: 20,
             height: 20,
+            speed: 12,
         },
         keys: {
             KeyA: false,//false, защото не е натиснат в началото;
